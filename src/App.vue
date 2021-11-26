46c5db29ace4e8a32bacff004e0c2ee57344b8d5
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-11-26 18:46:25 +0300

    add users

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import UsersList from '@/components/UsersList.vue';
+import {response} from '@/getUsers' 
 
 export default defineComponent({
   name: "App",
@@ -16,15 +17,13 @@ export default defineComponent({
   },
   data() {
     return {
-      LocalStorage: [
-        {"id":7,"email":"michael.lawson@reqres.in","first_name":"Michael","last_name":"Lawson","avatar":"https://reqres.in/img/faces/7-image.jpg"},
-        {"id":8,"email":"lindsay.ferguson@reqres.in","first_name":"Lindsay","last_name":"Ferguson","avatar":"https://reqres.in/img/faces/8-image.jpg"},
-        {"id":9,"email":"tobias.funke@reqres.in","first_name":"Tobias","last_name":"Funke","avatar":"https://reqres.in/img/faces/9-image.jpg"},
-        {"id":10,"email":"byron.fields@reqres.in","first_name":"Byron","last_name":"Fields","avatar":"https://reqres.in/img/faces/10-image.jpg"},
-        {"id":11,"email":"george.edwards@reqres.in","first_name":"George","last_name":"Edwards","avatar":"https://reqres.in/img/faces/11-image.jpg"},
-        {"id":12,"email":"rachel.howell@reqres.in","first_name":"Rachel","last_name":"Howell","avatar":"https://reqres.in/img/faces/12-image.jpg"}]
+      LocalStorage: []
     };
   },
+  async mounted() {
+    const result = await response;
+    this.LocalStorage = result.data;
+  }
 });
 </script>
 
---
+++ src/getUsers.ts
@@ -0,0 +1,4 @@
+export const response = fetch('https://reqres.in/api/users?page=2').then(res => res.json())
+
+
+ 
